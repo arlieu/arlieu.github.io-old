963d75c68699cf0da1894ee9b5c211d3af05d00a
Avery Lieu <avelieu@gmail.com>
2017-01-16 09:39:15 -0500

    form validations complete

---
+++ js/form-validation.js
@@ -0,0 +1,123 @@
+function submitContact() {
+	function validFirstName() {
+		var firstName = document.getElementById("first-name").value;
+		if (!/^[A-Za-z]+$/.test(firstName) && !/^\s*$/.test(firstName)) {
+			document.getElementById("error-label-first-name").innerHTML="*Alphabetical characters only";
+			return false;
+		}
+
+		document.getElementById("error-label-first-name").innerHTML="";
+		return true;
+	}
+
+	function validLastName() {
+		var lastName = document.getElementById("last-name").value;
+		if (!/^[A-Za-z]+$/.test(lastName) && !/^\s*$/.test(lastName)) {
+			document.getElementById("error-label-last-name").innerHTML="*Alphabetical characters only";
+			return false;
+		}
+
+		document.getElementById("error-label-last-name").innerHTML="";
+		return true;
+	}
+
+	function validEmail() {
+		var email = document.getElementById("sender-email").value;
+		if (!/[^\s@]+@[^\s@]+\.[^\s@]+/.test(email) && !/^\s*$/.test(email)) {
+			document.getElementById("error-label-email").innerHTML="*Valid email required";
+			return false;
+		}
+
+		document.getElementById("error-label-email").innerHTML="";
+		return true;
+	}
+
+	function validSubject() {
+		var subject = document.getElementById("subject").value;
+		if (!subject) {
+			document.getElementById("error-label-subject").innerHTML="*Subject required";
+			return false;
+		}
+
+		document.getElementById("error-label-subject").innerHTML="*";
+		return true;
+	}
+
+	function validMessage() {
+		var message = document.getElementById("message").value;
+		if (!message) {
+			document.getElementById("error-label-message").innerHTML="*Message required";
+			return false;
+		}
+
+		document.getElementById("error-label-message").innerHTML="*";
+		return true;
+	}
+
+	var check = [validFirstName(), validLastName(), validEmail(), validSubject(), validMessage()];
+
+	if (check[0] && check[1] && check[2] && check[3] && check[4]) {
+		alert("Message successfully sent!");
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+function submitRequest() {
+	function validFirstName() {
+		var firstName = document.getElementById("first-name").value;
+		if (!/^[A-Za-z]+$/.test(firstName)) {
+			document.getElementById("error-label-first-name").innerHTML="*Alphabetical characters only";
+			return false;
+		}
+
+		document.getElementById("error-label-first-name").innerHTML="*";
+		return true;
+	}
+
+	function validLastName() {
+		var lastName = document.getElementById("last-name").value;
+		if (!/^[A-Za-z]+$/.test(lastName)) {
+			document.getElementById("error-label-last-name").innerHTML="*Alphabetical characters only";
+			return false;
+		}
+
+		document.getElementById("error-label-last-name").innerHTML="*";
+		return true;
+	}
+
+	function validEmail() {
+		var email = document.getElementById("sender-email").value;
+		if (!/[^\s@]+@[^\s@]+\.[^\s@]+/.test(email)) {
+			document.getElementById("error-label-email").innerHTML="*Valid email required";
+			return false;
+		}
+
+		document.getElementById("error-label-email").innerHTML="*";
+		return true;
+	}
+
+	function validSubject() {
+		var subject = document.getElementById("subject").value;
+		if (!subject) {
+			document.getElementById("error-label-subject").innerHTML="*Select service";
+			return false;
+		}
+
+		document.getElementById("_subject").value=document.getElementById("subject").value
+		document.getElementById("error-label-subject").innerHTML="*";
+		return true;
+	}
+
+	var check = [validFirstName(), validLastName(), validEmail(), validSubject()];
+
+	if (check[0] && check[1] && check[2] && check[3]) {
+		alert("Message successfully sent!");
+		return true;
+	}
+	else {
+		return false;
+	}
+}
