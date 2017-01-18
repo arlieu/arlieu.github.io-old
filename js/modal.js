6b7cbb7e2cfc668b47664ef984938d2565954595
Avery Lieu <avelieu@gmail.com>
2017-01-17 23:42:07 -0500

    updated coach modals, linked LinkedIn

---
+++ js/modal.js
@@ -0,0 +1,44 @@
+$('#coach-1').click(function () {
+	document.getElementById('coach-picture-modal').src=document.getElementById('coach-picture-1').src;
+	document.getElementById('coachModalLabel').innerHTML=document.getElementById('coach-1').innerHTML;
+	document.getElementById('major').innerHTML="Computer Engineering";
+	document.getElementById('school').innerHTML="University of Virginia";
+	document.getElementById('coach-bio').innerHTML="I started off double majoring in computer science and applied math at the College of William & Mary. " +
+                                "During my free time, I would volunteer as a tutor for public schools in Williamsburg, VA, and " + 
+                                "eventually, I was hired as a tutor for W&M. I then became fascinated with computer engineering, " + 
+                                "which led me to transfer to the University of Virginia to study computer engineering. I am now a " +  
+                                "senior at UVA and have been privately tutoring for the past couple years with great success. " +
+                                "Please feel free to check out some of my <a href='https://www.wyzant.com/Tutors/STEM-Avery'>student reviews</a>.";
+});
+
+$('#coach-2').click(function () {
+	document.getElementById('coach-picture-modal').src=document.getElementById('coach-picture-2').src;
+	document.getElementById('coachModalLabel').innerHTML=document.getElementById('coach-2').innerHTML;
+	document.getElementById('major').innerHTML="Major";
+	document.getElementById('school').innerHTML="School";
+	document.getElementById('coach-bio').innerHTML="Coaches add descriptions highlighting their skills, qualifications, and background here.";
+});
+
+$('#coach-3').click(function () {
+	document.getElementById('coach-picture-modal').src=document.getElementById('coach-picture-3').src;
+	document.getElementById('coachModalLabel').innerHTML=document.getElementById('coach-3').innerHTML;
+	document.getElementById('major').innerHTML="Major";
+	document.getElementById('school').innerHTML="School";
+	document.getElementById('coach-bio').innerHTML="Coaches add descriptions highlighting their skills, qualifications, and background here.";
+});
+
+$('#coach-4').click(function () {
+	document.getElementById('coach-picture-modal').src=document.getElementById('coach-picture-4').src;
+	document.getElementById('coachModalLabel').innerHTML=document.getElementById('coach-4').innerHTML;
+	document.getElementById('major').innerHTML="Major";
+	document.getElementById('school').innerHTML="School";
+	document.getElementById('coach-bio').innerHTML="Coaches add descriptions highlighting their skills, qualifications, and background here.";
+});
+
+$('#coach-5').click(function () {
+	document.getElementById('coach-picture-modal').src=document.getElementById('coach-picture-5').src;
+	document.getElementById('coachModalLabel').innerHTML=document.getElementById('coach-5').innerHTML;
+	document.getElementById('major').innerHTML="Major";
+	document.getElementById('school').innerHTML="School";
+	document.getElementById('coach-bio').innerHTML="Coaches add descriptions highlighting their skills, qualifications, and background here.";
+});
