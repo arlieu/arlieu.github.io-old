cd8091c6c4e5334ec20378b9d8bff8a35031ba11
Avery Lieu <avelieu@gmail.com>
2017-01-31 23:54:21 -0500

    direct message

--- js/request-page.js
+++ js/request-page.js
@@ -1,7 +1,8 @@
 window.onload = function() {
 	url = window.location.search;
 	if (url) {
-		var coach = url.get("coach");
+		var tmp = url.split("=");
+		var coach = tmp[1];
 		coach = coach.replace(/_/g, " ");
 		coach += ".";
 		document.getElementById("coach-selection").value = coach;
